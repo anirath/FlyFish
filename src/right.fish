25f6c01f4af0ed12d0b363c0b073e5b5c1fdf716
Thomas Clark <anirath@pm.me>
2020-05-14 01:11:40 -0800

    Made some adjustments to the structure, and moved a few segments around between the right and left prompts. Also, adjusted some of the code in a minor way.

--- src/right.fish
+++ src/right.fish
@@ -1,30 +1,23 @@
-#!/usr/bin/env fish 
-# title       : Flyfish Prompt Right Script
+#!/usr/bin/env fish
+# title       : FlyFish Prompt: Right Prompt Function Script
 # author      : anirath
 # created     : 2019.11.18
-# modified    : 2020.05.01
-# usage       : sourced by the Fish Shell when FlyFish is installed.
+# modified    : 2020.05.14
+# usage       : autoloaded by `fish`
 # file        : `./right.fish`
-# description : right script file for the FlyFish prompt.
-# ==================================================================
-# Right Prompt Function
-# =====================
+# description : right script for the FlyFish prompt for `fish`.
+# =============================================================
+# Autoloaded Function -- fish_right_prompt()
+# ==========================================
 function fish_right_prompt -d "Write out the right prompt"
-    # Before the segments set $last_status and $is_git_repository to reflect if using git.
-    set -l last_status $status; and set -l is_git_repository (git rev-parse --is-inside-work-tree ^/dev/null)
     # --------------
-    # Status Segment
+    # Git Repository
     # --------------
-    if not test "$last_status" -eq 0
-        # If the last status is anything but 0 then set the error in this segment.
-        set_color -o red; and printf "%b" "\U203C Error Code: $last_status \U203C"; and set_color normal
-    end
-    # -----------
-    # Git Segment
-    # -----------
+    # Use a variable to detect if CWD is a git repository.
+    set -l is_git_repository (git rev-parse --is-inside-work-tree ^/dev/null)
     if test -n "$is_git_repository"
         # If the PWD is a git repository then print the git message and check the git status.
-        set_color A3A3A3; and echo -n "[git:"
+        set_color 999999; printf "[git:"
         set -l branch (git symbolic-ref --short HEAD ^/dev/null; or git show-ref --head -s --abbrev | head -n1 ^/dev/null)
         git diff-files --quiet --ignore-submodules ^/dev/null; or set -l has_unstaged_files
         git diff-index --quiet --ignore-submodules --cached HEAD ^/dev/null; or set -l has_staged_files
@@ -39,7 +32,7 @@ function fish_right_prompt -d "Write out the right prompt"
             set_color green
         end
         # Print the git branch into the segment with the previously formatted colors.
-        echo -n "$branch]"
+        printf "$branch"; set_color 999999
         # Set the variables to determine upstream status.
         git rev-parse --abbrev-ref '@{upstream}' >/dev/null; and set -l has_upstream
         if set -q has_upstream
@@ -49,15 +42,15 @@ function fish_right_prompt -d "Write out the right prompt"
             set -l commits_to_pull (echo $commit_counts | cut -f 2 ^/dev/null)
             # Test the commits to push and set the text color appropriately.
             if test $commits_to_push != 0
-                if test $commits_to_pull -ne 0 
+                if test $commits_to_pull -ne 0
                     set_color red
-                else if test $commits_to_push -gt 3 
+                else if test $commits_to_push -gt 3
                     set_color yellow
-                else 
+                else
                     set_color green
                 end
                 # Print the upstream push symbol with the set color.
-                echo -n " ⇡ "
+                printf "⇡"
             end
             # Test the commits to pull and set the text color appropriately.
             if test $commits_to_pull != 0
@@ -69,16 +62,20 @@ function fish_right_prompt -d "Write out the right prompt"
                     set_color green
                 end
                 # Print the upstream pull symbol with the set color.
-                echo -n " ⇣ "
+                printf "⇣"
             end
-            # Reset text formatting
-            set_color normal
         end
         if test (git stash list | wc -l) -gt 0
             # If there are any stashed changes print the corresponding symbol to the prompt.
-            set_color A3A3A3; and echo -n " ☰ "
+            set_color 999999; printf ":"; set_color white; printf "☰"
         end
-        # Reset text formatting.
-        set_color normal
+        set_color 999999; printf "]"; set_color normal
+    end
+    # --------------
+    # Python Segment
+    # --------------
+    if test (count *.py) -gt 0
+        # If any files with extension '.py' are present in the CWD then print out the Python Segment.
+        flyfish_separator; set_color -o green; printf "\U1F40D Python \U1F40D"; set_color normal
     end
 end
